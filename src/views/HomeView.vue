e757eaa4d19510bb048ca56538e652e5378eac0b
Afrijal Dzuhri <ijuldz@gmail.com>
2023-09-13 23:29:56 +0700

    init project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <h1 class="text-3xl font-bold underline text-green-500">
+      Hello world!
+    </h1>
+  </main>
+</template>
